2182a5a55f875dbef8e20bd083664cc7518c1083
JEONGSUJI <wtw9611@gmail.com>
2019-10-10 11:29:16 +0900

    15번 삼각형 출력 다른 방법

--- practice_ControlFlowStatement/practice15.js
+++ practice_ControlFlowStatement/practice15.js
@@ -1,21 +1,40 @@
 // 15. 정삼각형 출력하기
 
+// var result = '';
+
+// for (var i = 5; i > 0; i--){
+//   for (var j = 0; j < 5; j++) {
+//     if (i > j) {
+//       result += ' ';
+//     } else {
+//       result += '*';
+//     }
+//   }
+
+//   result += '*';
+
+//   for (var j = 0; j < 5; j++) {
+//     if (i <= j) result += '*';
+//   }
+//   result += '\n';
+// }
+
+// console.log(result);
+
+
+// 공백 4 3 2 1 0, 별 1 3 5 7 9 증가
+
 var result = '';
 
-for (var i = 5; i > 0; i--){
-  for (var j = 0; j < 5; j++) {
-    if (i > j) {
-      result += ' ';
-    } else {
-      result += '*';
-    }
+for (var i = 0; i < 5; i++){
+  for (var j = 4; j > i; j--){
+    result += ' ';
   }
 
-  result += '*';
-
-  for (var j = 0; j < 5; j++) {
-    if (i <= j) result += '*';
+  for (var k = 1; k <= ((i * 2) + 1); k++){
+    result += '*';
   }
+
   result += '\n';
 }
 
