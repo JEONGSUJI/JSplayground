337194d2d7779916e6aac752ea591042c72b1541
JEONGSUJI <wtw9611@gmail.com>
2019-11-11 00:29:01 +0900

    todo list server 재연습 - method 모듈화 하기

--- todo-list-server-exercise/public/js/index.js
+++ todo-list-server-exercise/public/js/index.js
@@ -20,23 +20,9 @@ const render = (data) => {
   $todos.innerHTML = html;
 };
 
-const getTodo = (url, f) => {
+const method = (func, url, f, payload) => {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200){
-      f(JSON.parse(xhr.response));
-    } else {
-      throw new Error(xhr.status);
-    }
-  };
-};
-
-const post = (url, f, payload) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
+  xhr.open(func, url);
   xhr.setRequestHeader('Content-type', 'application/json');
   xhr.send(JSON.stringify(payload));
   xhr.onreadystatechange = () => {
@@ -49,58 +35,87 @@ const post = (url, f, payload) => {
   };
 };
 
-const del = (url, f, payload) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('DELETE', url);
-  xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.send(JSON.stringify(payload));
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200){
-      f(JSON.parse(xhr.response));
-    } else {
-      throw new Error(xhr.status);
-    }
-  };
-};
+// const getTodo = (url, f) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', url);
+//   xhr.send();
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//     if (xhr.status === 200){
+//       f(JSON.parse(xhr.response));
+//     } else {
+//       throw new Error(xhr.status);
+//     }
+//   };
+// };
 
-const patch = (url, f, payload) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('PATCH', url);
-  xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.send(JSON.stringify(payload));
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200){
-      f(JSON.parse(xhr.response));
-    } else {
-      throw new Error(xhr.status);
-    }
-  };
-};
+// const post = (url, f, payload) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('POST', url);
+//   xhr.setRequestHeader('Content-type', 'application/json');
+//   xhr.send(JSON.stringify(payload));
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//     if (xhr.status === 200){
+//       f(JSON.parse(xhr.response));
+//     } else {
+//       throw new Error(xhr.status);
+//     }
+//   };
+// };
+
+// const del = (url, f, payload) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('DELETE', url);
+//   xhr.setRequestHeader('Content-type', 'application/json');
+//   xhr.send(JSON.stringify(payload));
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//     if (xhr.status === 200){
+//       f(JSON.parse(xhr.response));
+//     } else {
+//       throw new Error(xhr.status);
+//     }
+//   };
+// };
+
+// const patch = (url, f, payload) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('PATCH', url);
+//   xhr.setRequestHeader('Content-type', 'application/json');
+//   xhr.send(JSON.stringify(payload));
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//     if (xhr.status === 200){
+//       f(JSON.parse(xhr.response));
+//     } else {
+//       throw new Error(xhr.status);
+//     }
+//   };
+// };
 
 const addTodo = ({ target, keyCode }) => {
   const content = $input.value.trim();
   if (keyCode !== 13 || content === '') return;
   target.value = '';
   const maxId = todos.length ? Math.max(...todos.map((todo) => todo.id)) + 1 : 1;
-  post('/todos', render, { id: maxId, content, completed: false });
+  method('POST', '/todos', render, { id: maxId, content, completed: false });
 };
 
 const removeTodo = ({ target }) => {
   if (!target.classList.contains('remove')) return;
   const id = +target.parentNode.id;
-  del(`/todos/${id}`, render);
+  method('DELETE', `/todos/${id}`, render);
 };
 
 const checkTodo = ({ target }) => {
   const id = +target.parentNode.parentNode.id;
   const completed = !todos.find((todo) => todo.id === +id).completed;
-  patch(`/todos/${id}`, render, { completed });
+  method('PATCH', `/todos/${id}`, render, { completed });
 };
 
 window.onload = () => {
-  getTodo('/todos', render);
+  method('GET', '/todos', render);
 };
 
 $input.onkeyup = (e) => {
